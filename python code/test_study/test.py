71fd29b94c0734a1cf33cb7c64b759d0dcd9a69f
puffyyy <ydl57913@163.com>
2020-09-13 21:24:48 +0800

    update

--- python code/test_study/test.py
+++ python code/test_study/test.py
@@ -1,23 +1,39 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+class Dict(dict):
+    '''
+    Simple dict but also support access as x.y style.
 
-' a test module '
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    '''
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
 
-__author__ = 'puffyoung'
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
 
-import sys
-
-
-def test():
-    args = sys.argv
-    if len(args) == 1:
-        print('Hello, world!')
-    elif len(args) == 2:
-        print('Hello, %s!' % args[1])
-    else:
-        print('Too many arguments!')
+    def __setattr__(self, key, value):
+        self[key] = value
 
 
 if __name__ == '__main__':
-    test()
-    print(sys.path)
+    import doctest
+    doctest.testmod()
