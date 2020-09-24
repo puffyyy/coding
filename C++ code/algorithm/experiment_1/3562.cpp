6682ac6946656196bf31a98c3d6a2414d69fdea5
puffyyy <ydl57913@163.com>
2020-09-24 20:05:04 +0800

    update

---
+++ C++ code/algorithm/experiment_1/3562.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ll a, b;
+    int n;
+    while (~scanf("%d%lld%lld", &n, &a, &b))
+    {
+        }
+    return 0;
+}
