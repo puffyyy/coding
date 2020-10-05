2cb25fc53249d6a43ef6116c6d6fc6af4d0ebbdd
puffyyy <ydl57913@163.com>
2020-10-05 17:09:30 +0800

    update

--- C code/Function_Modules/binarysearch/binarysearch.c
+++ C code/Function_Modules/binarysearch/binarysearch.c
@@ -1,43 +1,66 @@
 #include <stdio.h>
 #include <string.h>
 int a[100001];
-int binarysearch_left(int lenth,int x)
+int binarysearch_left(int lenth, int x)
 {
-    int left=0,right=lenth-1;
-    while(left<=right){
-        int mid=left+((right-left)>>1);
-        if(a[mid]>=x)
-            right=mid-1;
-        else left=mid+1;
+    int left = 0, right = lenth - 1;
+    while (left <= right)
+    {
+        int mid = left + ((right - left) >> 1);
+        if (a[mid] >= x)
+            right = mid - 1;
+        else
+            left = mid + 1;
     }
-    if(a[left]==x)return left;
-    else return -1;
+    if (a[left] == x)
+        return left;
+    else
+        return -1;
 }
-int binarysearch_right(int lenth,int x)
+int binarysearch_right(int lenth, int x)
 {
-    int left=0,right=lenth-1,mid;
-    while(left<=right){
-        mid=left+((right-left)>>1);
-        if(a[mid]<=x)
-            left=mid+1;
-        else right=mid-1;
+    int left = 0, right = lenth - 1, mid;
+    while (left <= right)
+    {
+        mid = left + ((right - left) >> 1);
+        if (a[mid] <= x)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
-    if(a[right]==x)return right;
-    else return -1;
+    if (a[right] == x)
+        return right;
+    else
+        return -1;
+}
+int binarysearchleftcnt(int x, int len) //小于x的个数 要求升序 下标从0开始
+{
+    int mid, l, r;
+    l = 0, r = len;
+    while (l < r)
+    {
+        mid = (l + r) >> 1;
+        if (a[mid] < x)
+            l = mid + 1;
+        else
+            r = mid;
+    }
+    return l;
 }
 int main()
-{   
-    int n,i,t;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
-    scanf("%d",&t);
-    for(;t>0;t--)
+{
+    int n, i, t;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    scanf("%d", &t);
+    for (; t > 0; t--)
     {
-        scanf("%d",&i);
-        if(binarysearch_left(n,i)!=-1)
-        printf("%d %d\n",binarysearch_left(n,i)+1,binarysearch_right(n,i)+1);
-        else printf("-1\n");
+        scanf("%d", &i);
+        if (binarysearch_left(n, i) != -1)
+            printf("%d %d\n", binarysearch_left(n, i) + 1, binarysearch_right(n, i) + 1);
+        else
+            printf("-1\n");
     }
 
     return 0;
