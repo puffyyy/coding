4899ee662cd08cc031adffea7acf929c82dc6f16
puffyyy <ydl57913@163.com>
2020-10-05 13:28:50 +0800

    update

---
+++ C++ code/algorithm/exercise_3/d_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    //ios::sync_with_stdio(false);
+
+    return 0;
+}
---
+++ C++ code/algorithm/exercise_3/i.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+typedef long long int ll;
+const int maxm = 1e5 + 5;
+double treesum[maxm];
+int treecnt[maxm];
+int n;
+struct Star
+{
+    double val, dis;
+    int pos;
+    bool operator<(const Star &o) const
+    {
+        return (val == o.val) ? (pos < o.pos) : (val < o.val);
+    }
+};
+bool cmpdis(const Star &a, const Star &b)
+{
+    return a.dis < b.dis;
+}
+int lowbit(int x)
+{
+    return x & (-x);
+}
+void updatesum(int i, double k)
+{
+    while (i <= n)
+    {
+        treesum[i] += k;
+        i += lowbit(i);
+    }
+}
+void updatecnt(int i, int k)
+{
+    while (i <= n)
+    {
+        treecnt[i] += k;
+        i += lowbit(i);
+    }
+}
+double getsum(int i)
+{
+    //求A[1 - i]的和
+    double res = 0;
+    while (i > 0)
+    {
+        res += treesum[i];
+        i -= lowbit(i);
+    }
+    return res;
+}
+int getcnt(int i)
+{
+    //求A[1 - i]的个数
+    int res = 0;
+    while (i > 0)
+    {
+        res += treecnt[i];
+        i -= lowbit(i);
+    }
+    return res;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    Star *stars = new Star[n + 1];
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> stars[i].val >> stars[i].dis;
+    }
+    sort(stars + 1, stars + 1 + n, cmpdis);
+    for (int i = 1; i <= n; ++i)
+        stars[i].pos = i;
+    sort(stars + 1, stars + 1 + n);
+    double ans = 0;
+    double sum_to_i = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        int lcnt = getcnt(stars[i].pos);
+        double lsum = getsum(stars[i].pos);            // 在i左侧的离原点坐标和
+        double lsum_to_i = lcnt * stars[i].dis - lsum; // 在i左侧的离当前点i 的距离和
+        double rsum_to_i = sum_to_i - lsum - (i - 1 - lcnt) * stars[i].dis;
+        ans += (lsum_to_i + rsum_to_i) * stars[i].val;
+
+        updatecnt(stars[i].pos, 1);
+        updatesum(stars[i].pos, stars[i].dis);
+        sum_to_i += stars[i].dis;
+    }
+    printf("%.3lf", ans);
+    return 0;
+}
